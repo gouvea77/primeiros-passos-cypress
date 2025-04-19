f369c38d82e0f8991ba0d40540b30bb05331eb8b
gouvea77 <gabrielgouvea77@hotmail.com>
2025-04-18 23:14:27 -0300

    base url added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -9,10 +9,8 @@ describe('Orange HRM Tests', () => {
     wrongCredentialAlert: "[role='alert']"
   }
 
-
-
   it('Login - Success', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorList.loginButton).click()
@@ -20,7 +18,7 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorList.dashboardGrid)
   })
   it('Login - Failed', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorList.usernameField).type(userData.userFail.username)
     cy.get(selectorList.passwordField).type(userData.userFail.password)
     cy.get(selectorList.loginButton).click()
