0ae500ff409ca91a6c568c6e1f1515a85676a16a
gouvea77 <gabrielgouvea77@hotmail.com>
2025-04-26 12:19:08 -0300

    Added page objects in My info page

--- cypress/pages/myInfoPage.js
+++ cypress/pages/myInfoPage.js
@@ -21,12 +21,24 @@ class myInfoPage {
     cy.get(this.selectorList().myInfoButton).click()
   }
 
-  // writeFields(username, password){
-  //   cy.get(this.selectorList().usernameField).type(username)
-  //   cy.get(this.selectorList().passwordField).type(password)
-  //   cy.get(this.selectorList().loginButton).click()
+  writeFields(username, password){
+    cy.get(this.selectorList().firstName).clear().type("Gabriel")
+    cy.get(this.selectorList().middleName).clear()
+    cy.get(this.selectorList().lastName).clear().type("Gouvea")
+    cy.get(this.selectorList().genericField).eq(3).clear().type("256804")
+    cy.get(this.selectorList().genericField).eq(4).clear().type("77")
+    cy.get(this.selectorList().genericField).eq(5).clear().type("DriveLicense29")
+    cy.get(this.selectorList().dateField).eq(0).clear().type("2025-13-04")
+    cy.get(this.selectorList().dateCloseButton).click()
+    cy.get(this.selectorList().dateField).eq(1).clear().type("2025-13-04")
+    cy.get(this.selectorList().dateCloseButton).click()
+    cy.get(this.selectorList().saveButton).click()
+    cy.get(this.selectorList().genericCombobox).eq(0).click().contains("Brazilian").click()
+    cy.get(this.selectorList().genericCombobox).eq(1).click().contains("Single").click({force: true})
+    cy.get(this.selectorList().radioOption).eq(0).click()
+    cy.get(this.selectorList().genericCombobox).eq(2).click().contains("B+").click()
 
-  // }
+  }
 }
 
 export default myInfoPage
